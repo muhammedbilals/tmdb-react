e7aada19e9e931435d0401b8d3d71258f7d6234b
Muhammed Bilal S <bilalmuhammed363@gmail.com>
2024-08-17 17:38:00 +0530

    edited

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = 'c2e2608024beabac09aa26c5fb840229';
+const apiKey = 'apikey';
 
 const tmdbApi = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
